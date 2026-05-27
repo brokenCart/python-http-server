3307958f134f35c2ec04e896708fe1854c723430
brokenCart <yathartgpta@gmail.com>
2026-05-27 17:45:09 +0530

    feat: added another route for testing JSONResponse

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from http_server.server import HTTPServer
-from http_server.response import TextResponse
+from http_server.response import TextResponse, JSONResponse
 
 serv = HTTPServer("localhost", 8000)
 
 @serv.get("/")
 def hello(request):
     return TextResponse(200, body=b"Hello, World!")
+
+@serv.get("/json/")
+def hello_json(request):
+    return JSONResponse(200, body={
+        "message": "Hello, World!",
+        "username": "brokenCart"
+    })
 
 serv.start()
